e658c96657c2f53ae3f580764fcd3c1c40289a02
frankpengyuan <frankpengyuan@gmail.com>
2015-01-26 01:25:27 +0800

    cachedMat

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## chahed matrix which can store its inverse matrix, thus when calculating
+## its inverse for the second time, we can use the stored value.
 
-## Write a short comment describing this function
+## constructor of the cached matrix, with four methods, namely
+## set, get, setinv, getinv
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(ori_mat){
+    x <<- ori_mat
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(out_inv) inv <<- out_inv
+  getinv <- function() inv
+  list(set = set,
+       get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## solve the inverse matrix of given cached matrix, using 
+## the stored value if avaliable.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
 }
